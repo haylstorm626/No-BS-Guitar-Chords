aac9b14b2d5ef959b1fc9f6c016f682364c722a5
Hayley Knight <keyb0ardf4ilure@gmail.com>
2019-09-11 16:13:15 -0400

    Finish search function, edit css, add more chords

---
+++ js/search.js
@@ -0,0 +1,14 @@
+	$(document).ready(function(){
+		$('#search').keyup(function(){
+			$('#result').html('');
+			var searchField = $('#search').val();
+			var expression = new RegExp(searchField, "i");
+			$.getJSON('chords.json', function(chords){
+				$.each(chords, function(key, value){
+					if(value.name.search(expression) != -1){
+						$('#result').append('<li><a href="chord.html?chord=' +key+ '">' +value.name+ '</a></li>');
+					}
+				});
+			});
+		});
+	});
